167377b206fae044e5b99313c63d009c2039b318
Gnaseel <hgnaseel@gmail.com>
2020-06-08 18:10:31 +0900

    Link express with vue

--- backend/app.js
+++ backend/app.js
@@ -1,50 +1,18 @@
 const express = require('express');
 const app =express();
 app.use(express.static('public'));
+var userRouter = require('./routes/users.js');
 
-
-
-app.get('/',(req,res)=>res.send('Hello, world?'))
+app.get('/',(req,res)=>res.send('aa, world?'))
+app.get('/a',(req,res)=>res.send('Hello, world?'))
 app.get('/abc',function(req,res) {
-  var html ='abcde<img src="./img/logo.png">';
+  var html ='abcde<img src="./public/img/logo.png">';
   res.send(html);
 });
-app.removeListener(3000,()=> console.log('example App'));
+//app.removeListener(3000,()=> console.log('example App'));
 app.get('/arhen/:abc',(req, res)=> res.send('arhentina<br >'+req.params.abc))
-app.get('update',function(req,res){
 
-})
 
-// var createError = require('http-errors');
-// var express = require('express');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-// var app = express();
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+app.use('/api/user',userRouter);
 
 module.exports = app;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var userList = require('../temp.json');
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(userList);
 });
+router.get('/:id',function(req,res){
+  var str="";
+  var len = userList.length;
 
+  for(var i=0;i<len;i++){
+    if(req.params.id==userList[i].id)
+      str+=JSON.stringify(userList[i]);
+  }
+  res.send(str);
+})
 module.exports = router;

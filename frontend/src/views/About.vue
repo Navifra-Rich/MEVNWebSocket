231728718766e002dac90dfcfc7b77fbb714506f
Gnaseel <hgnaseel@gmail.com>
2020-06-08 18:23:45 +0900

    update frontend

---
+++ frontend/src/views/About.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+    <h1>ABOUT!!!!!!!!!!!!!!!</h1>
+  
+    {{tt}}
+  </div>
+</template>
+<script>
+export default {
+  created(){
+    if(this.$route.params.id!=null){
+      var id = this.$route.params.id;
+      this.$http.get('/api/user/'+id)
+          .then((response) => {
+            this.tt = response.data
+      })
+    }else{
+       this.$http.get('/api/user')
+          .then((response) => {
+            this.tt = response.data
+            })
+    }
+  },
+ data () {
+    return {
+      tt: []
+    }
+  }
+}
+</script>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home">
+   HOME!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<br>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
